fe74289f8c5ed5f0da2b9cf8968f78e4001d03a9
vicanso <vicansocanbico@gmail.com>
2016-05-04 22:06:24 +0800

    use `eslint`

--- Readme.md
+++ Readme.md
@@ -1,5 +1,10 @@
 # koa-rest-version
 
+[![Build Status](https://travis-ci.org/vicanso/koa-rest-version.svg?style=flat-square)](https://travis-ci.org/vicanso/koa-rest-version)
+[![Coverage Status](https://img.shields.io/coveralls/vicanso/koa-rest-version/master.svg?style=flat)](https://coveralls.io/r/vicanso/koa-rest-version?branch=master)
+[![npm](http://img.shields.io/npm/v/koa-rest-version.svg?style=flat-square)](https://www.npmjs.org/package/koa-rest-version)
+[![Github Releases](https://img.shields.io/npm/dm/koa-rest-version.svg?style=flat-square)](https://github.com/vicanso/koa-rest-version)
+
 parse rest api version config, support two url format:
 
 - `/v2/user/me?_type=json`
@@ -14,9 +19,9 @@ const version = require('version');
 const app = new Koa();
 app.use(version());
 app.use(ctx => {
-	// url: /user/me
-	// {"version": 2, "type": "xml"}
-	console.info(ctx.versionConfig);
+  // url: /user/me
+  // {"version": 2, "type": "xml"}
+  console.info(ctx.versionConfig);
 });
 app.listen();
 // GET /v2/user/me?_type=xml
--- index.js
+++ index.js
@@ -1,43 +1,43 @@
 'use strict';
-
-module.exports = version;
-
 /**
  * [version description]
  * @return {[type]} [description]
  */
 function version() {
-	return (ctx, next) => {
-		const query = ctx.query;
-		const currentPath = ctx.path;
-		const reg = /^\/v\d+/i;
-		const result = reg.exec(currentPath);
-		const versionConfig = {};
-		if (result) {
-			const versionDesc = result[0];
-			const version = parseInt(versionDesc.substring(2));
-			ctx.path = ctx.path.substring(versionDesc.length);
-			versionConfig.version = version;
-		}
-		if (query._type) {
-			versionConfig.type = query._type;
-			delete query._type;
-			ctx.query = query;
-		}
-		const acceptReg = /^application\/vnd\.\S+\.v(\d)+(\+(\S+))?/i;
-		const acceptResult = acceptReg.exec(ctx.get('Accept'));
-		/* istanbul ignore else */
-		if (acceptResult) {
-			versionConfig.version = parseInt(acceptResult[1]);
-			/* istanbul ignore else */
-			if (acceptResult[3]) {
-				versionConfig.type = acceptResult[3];
-			}
-		}
-		/* istanbul ignore else */
-		if (versionConfig.version) {
-			ctx.versionConfig = versionConfig;
-		}
-		return next();
-	};
+  return (ctx, next) => {
+    const cloneCxt = ctx;
+    const query = ctx.query;
+    const currentPath = ctx.path;
+    const reg = /^\/v\d+/i;
+    const result = reg.exec(currentPath);
+    const versionConfig = {};
+    if (result) {
+      const versionDesc = result[0];
+      const v = parseInt(versionDesc.substring(2), 10);
+      cloneCxt.path = cloneCxt.path.substring(versionDesc.length);
+      versionConfig.version = v;
+    }
+    if (query._type) {
+      versionConfig.type = query._type;
+      delete query._type;
+      cloneCxt.query = query;
+    }
+    const acceptReg = /^application\/vnd\.\S+\.v(\d)+(\+(\S+))?/i;
+    const acceptResult = acceptReg.exec(ctx.get('Accept'));
+    /* istanbul ignore else */
+    if (acceptResult) {
+      versionConfig.version = parseInt(acceptResult[1], 10);
+      /* istanbul ignore else */
+      if (acceptResult[3]) {
+        versionConfig.type = acceptResult[3];
+      }
+    }
+    /* istanbul ignore else */
+    if (versionConfig.version) {
+      cloneCxt.versionConfig = versionConfig;
+    }
+    return next();
+  };
 }
+
+module.exports = version;
